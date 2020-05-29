ccf9f10d70aa7af6f5692c37451d3eee4ac0e672
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-05-29 09:50:33 -0400

    Update pgtable.sql

--- pgtable.sql
+++ pgtable.sql
@@ -1,12 +1,13 @@
 create table prices
 (
-item_id serial PRIMARY KEY,
-price FLOAT,
+item_id integer NOT NULL,
+price FLOAT NOT NULL,
 created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-updated_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+updated_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (item_id, price)
 );
 
 insert into prices
-(price)
+(item_id, price)
 values
-(9.99);
+(1, 9.99);
