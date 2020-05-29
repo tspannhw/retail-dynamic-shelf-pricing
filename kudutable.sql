cd0159d191657550bbbc1d2d8c6757d53deaaa66
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-05-29 09:40:03 -0400

    Update kudutable.sql

--- kudutable.sql
+++ kudutable.sql
@@ -20,7 +20,7 @@ uuid STRING,
 	cputemp STRING, 
 	diskfree STRING, 
 	memory DOUBLE, 
-  price STRING
+  price STRING,
  PRIMARY KEY (uuid) ) 
 PARTITION BY HASH PARTITIONS 4 
 STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
