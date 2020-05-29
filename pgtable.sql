86d742cf3dc288b66be5323ef02bc11b63c7c606
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-05-29 09:53:09 -0400

    Update pgtable.sql

--- pgtable.sql
+++ pgtable.sql
@@ -4,10 +4,16 @@ item_id integer NOT NULL,
 price FLOAT NOT NULL,
 created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 updated_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (item_id, price)
+  PRIMARY KEY (item_id)
 );
 
 insert into prices
 (item_id, price)
 values
 (1, 9.99);
+
+update prices
+set price = 10.99
+where item_id = 1;
+
+SELECT * FROM "public"."prices" LIMIT 100
