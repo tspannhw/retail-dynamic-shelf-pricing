8c1e68bba59451cff141af779cf283b45c3227c5
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-07-16 17:05:05 -0400

    Update kudutable.sql

--- kudutable.sql
+++ kudutable.sql
@@ -1,3 +1,30 @@
+  "longDescription" : "Eggo Homestyle Waffles 10 Count",
+  "brandName" : "Eggo",
+  "displayImage" : "https://www.kroger.com/product/images/small/left/0003800040260",
+  "price" : "2.59",
+  "msrp" : "2.59",
+  "tpr" : "0",
+  "update_dt" : "20200716165641",
+  "upc" : "0003800040260",
+  "itemDescription" : "Eggo Homestyle Waffles 10 Count",
+  "origin_store" : "kroger"
+  
+CREATE TABLE 
+(
+  upc STRING,
+  origin_store STRING,
+  longDescription STRING,
+  itemDescription STRING,
+  brandName STRING,
+  displayImage STRING, 
+  price FLOAT,
+  msrp FLOAT,
+  tpr FLOAT,
+  update_dt TIMESTAMP,
+PRIMARY KEY (upc, origin_store) ) 
+PARTITION BY HASH PARTITIONS 4 
+STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
+
 CREATE TABLE itemprice
 (
        item_id INT,
