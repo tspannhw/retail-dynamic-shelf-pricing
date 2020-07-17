97344048a5cd9e819e26d4becdd2a67b3d18b136
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-07-17 16:26:00 -0400

    Update kudutable.sql

--- kudutable.sql
+++ kudutable.sql
@@ -32,7 +32,7 @@ CREATE TABLE prices
   msrp STRING,
   tpr STRING,
   updateDate STRING,
-PRIMARY KEY (upc, origin_store) ) 
+PRIMARY KEY (upc, originStore) ) 
 PARTITION BY HASH PARTITIONS 4 
 STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
 
