80882736892cdf010a8b07658b4fd4eb61eaada3
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-07-27 16:07:56 -0400

    Update flink.sql

--- flink.sql
+++ flink.sql
@@ -9,6 +9,23 @@ describe itemprice;
 select * from itemprice
 where originstore = 'kroger';
 
-select * from itemprice
+select upc,updatedate,itemdescription,brandname,CAST(price as float) as price, UUID() as uuid
+from itemprice
 where originstore = 'walmart';
+
+
+select upc,updatedate,itemdescription,brandname,CAST(price as float) as price, UUID() as uuid
+from itemprice
+where originstore = 'kroger';
+
+insert into krogerprices
+select upc,updatedate,itemdescription,brandname,CAST(price as float) as price, UUID() as uuid
+from itemprice
+where originstore = 'kroger';
+
+insert into walmartprices
+select upc,updatedate,itemdescription,brandname,CAST(price as float) as price, UUID() as uuid
+from itemprice
+where originstore = 'walmart';
+
 
