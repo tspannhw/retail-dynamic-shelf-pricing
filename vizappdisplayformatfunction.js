bfe673fd0de7e3877e3d3099741db2e7a42d4cfe
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-07-27 14:10:22 -0400

    Create vizappdisplayformatfunction.js
    for display image

---
+++ vizappdisplayformatfunction.js
@@ -0,0 +1,5 @@
+function myFunc(value) {
+    // Your custom JS code goes here
+    // Make sure to return an updated version of value
+    return '<img height="100" src="' + value + '" alt="' + value + '">';
+  }
