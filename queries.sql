ef46e5e84014652fe04ee51abd60895b161346d8
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-07-27 15:25:36 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,15 @@
 select upc, originstore, updatedate, brandname, price, itemdescription, displayimage, longdescription, msrp, tpr
 from itemprice
-order by updatedate desc
+order by updatedate desc;
+
+select upc, originstore, updatedate, brandname, price, itemdescription, displayimage, longdescription, msrp, tpr
+from itemprice
+where brandname = 'Oreo'
+order by brandname asc,updatedate desc;
+
+select upc, brandname, originstore
+from itemprice
+group by brandname, upc, originstore
+order by brandname asc;
+
+
