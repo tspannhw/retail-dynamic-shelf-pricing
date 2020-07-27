92a3bda789befe61341eb903455018fb036033d5
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-07-27 15:44:26 -0400

    Create flink.sql

---
+++ flink.sql
@@ -0,0 +1,14 @@
+show catalogs;
+
+use catalog registry;
+
+show tables;
+
+describe itemprice;
+
+select * from itemprice
+where originstore = 'kroger';
+
+select * from itemprice
+where originstore = 'walmart';
+
